2436a196a60ad88132e09ee78227968fdce01d3d
n1tr0xs <niko4pok@gmail.com>
2025-04-09 10:40:49 +0300

    Player renders as box for now. Floor coordinates changed

--- src/Player.cpp
+++ src/Player.cpp
@@ -2,11 +2,11 @@
 
 Player::Player(float x, float y, float width, float height, SDL_Texture* texture) : Entity(x, y, width, height, texture) {
 	int fw = 256;
-	int fh = 256;
-	addAnimation("idle", 0, 4, fw, fh);
-	addAnimation("running", 1, 4, fw, fh);
-	addAnimation("jumping", 2, 4, fw, fh);
-	addAnimation("crouching", 3, 4, fw, fh);
+	int fh = 128;
+	//addAnimation("idle", 0, 4, fw, fh);
+	//addAnimation("running", 1, 4, fw, fh);
+	//addAnimation("jumping", 2, 4, fw, fh);
+	//addAnimation("crouching", 3, 4, fw, fh);
 	m_current_animation = m_animations["idle"];
 }
 
@@ -43,12 +43,25 @@ void Player::update(Uint64 deltaTime) {
 	m_rect.y += m_y_velocity * deltaTime;
 
 	// Check if player has landed on the ground (simple ground check)
-	if (m_rect.y >= 400.0f) {  // Example ground level (y = 400)
-		m_rect.y = 400.0f;  // Snap to the ground level
+	float floor = 32*25;
+	if (m_rect.y >= floor){  // Example ground level (y = 400)
+		m_rect.y = floor;  // Snap to the ground level
 		m_on_ground = true;
 		m_y_velocity = 0;
 	}
 
 
 	updateAnimationFrame(deltaTime);
+}
+
+void Player::render(SDL_Renderer* renderer) {
+	SDL_SetRenderDrawColor(renderer, 255, 100, 100, 255);
+	SDL_FRect rect = {
+		m_rect.x,
+		m_rect.y,
+		m_rect.width,
+		m_rect.height
+	};
+	//SDL_RenderTexture(renderer, m_texture, NULL, &rect);
+	SDL_RenderFillRect(renderer, &rect);
 }
--- src/Player.h
+++ src/Player.h
@@ -7,9 +7,10 @@ public:
 	Player(float x, float y, float width, float height, SDL_Texture* texture);
 	
 	void update(Uint64 deltaTime) override;
+	void render(SDL_Renderer* renderer) override;
 private:
 	float m_x_vel = .5;
-	float m_jump_strength = -2;
+	float m_jump_strength = -2.6;
 	float m_y_velocity = 0;
 	bool m_on_ground = false;
 };
