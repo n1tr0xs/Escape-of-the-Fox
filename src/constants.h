d48f9f6708f58bca57dadfe3a32d5dc8051b52a2
n1tr0xs <niko4pok@gmail.com>
2025-04-27 22:12:44 +0300

    constants now really constant :)

--- src/constants.h
+++ src/constants.h
@@ -1,10 +1,10 @@
 #pragma once
 
-constexpr int TILE_SIZE = 32; // Tile size in pixels
-constexpr int RENDERER_HEIGHT_IN_TILES = 20; // Renderer height in tiles
-constexpr int RENDERER_WIDTH_IN_TILES = 32;  // Renderer width in tiles
-constexpr int RENDERER_HEIGHT_IN_PIXELS = RENDERER_HEIGHT_IN_TILES * TILE_SIZE; // Renderer height in pixels
-constexpr int RENDERER_WIDTH_IN_PIXELS = RENDERER_WIDTH_IN_TILES * TILE_SIZE; // Renderer width in pixels
+constexpr const int TILE_SIZE = 32; // Tile size in pixels
+constexpr const int RENDERER_HEIGHT_IN_TILES = 20; // Renderer height in tiles
+constexpr const int RENDERER_WIDTH_IN_TILES = 32;  // Renderer width in tiles
+constexpr const int RENDERER_HEIGHT_IN_PIXELS = RENDERER_HEIGHT_IN_TILES * TILE_SIZE; // Renderer height in pixels
+constexpr const int RENDERER_WIDTH_IN_PIXELS = RENDERER_WIDTH_IN_TILES * TILE_SIZE; // Renderer width in pixels
 
-constexpr char ASSET_PATH[] = "assets/{}";
-constexpr char LEVEL_ASSET_PATH[] = "assets/level_{:02d}/{}";
+constexpr const char* ASSET_PATH = "assets/{}";
+constexpr const char* LEVEL_ASSET_PATH = "assets/level_{:02d}/{}";
