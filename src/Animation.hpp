7d1e82fec70ab8cbb7e472c792892adb48ead411
n1tr0xs <niko4pok@gmail.com>
2025-05-09 11:21:35 +0300

    Animation getters & setters update.

--- src/Animation.hpp
+++ src/Animation.hpp
@@ -7,8 +7,14 @@ public:
 	Animation(int row, int numFrames, float frameWidth, float frameHeight);
 
 	// Getters
-	const SDL_FRect getFRect(int frameNum) const;
+	SDL_FRect getFRect(int frameNum) const { return { frameNum * m_frameWidth, m_row * m_frameHeight, m_frameWidth, m_frameHeight }; }
+	float getFrameWidth() const { return m_frameWidth; }
+	float getFrameHeight() const { return m_frameHeight; }
 	int getFrameCount() const { return m_numFrames; }
+	// Setters
+	void setFrameWidth(float width) { m_frameWidth = width; }
+	void setFrameHeight(float height) { m_frameHeight = height; }
+	void setFrameCount(int frameCount) { m_numFrames = frameCount; }
 private:
 	int m_row, m_numFrames;
 	float m_frameWidth, m_frameHeight;
