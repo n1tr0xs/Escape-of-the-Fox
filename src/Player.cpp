9b350734eb5424f7c82bab30e783487ea087e2a1
n1tr0xs <niko4pok@gmail.com>
2025-05-09 12:14:04 +0300

    player texture update

--- src/Player.cpp
+++ src/Player.cpp
@@ -2,9 +2,9 @@
 
 Player::Player(float x, float y, float width, float height, SDL_Texture* texture) :
 	Entity{ x, y, width, height, texture } {
-	float fw = 256;
-	float fh = 128;
-	addAnimation("idle", 0, 4, fw, fh);
+	float fw = 512;
+	float fh = fw / 2;
+	addAnimation("idle", 0, 1, fw, fh);
 	//addAnimation("running", 1, 4, fw, fh);
 	//addAnimation("jumping", 2, 4, fw, fh);
 	//addAnimation("crouching", 3, 4, fw, fh);
