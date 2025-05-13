20be3e2fff75007d6b0d220440c9e9d862e9195d
Programmist <niko4pok@gmail.com>
2025-05-13 16:01:22 +0300

    aliases now in separate file

--- src/Engine.hpp
+++ src/Engine.hpp
@@ -10,9 +10,7 @@
 #include "ResourceManager.hpp"
 #include "Scene.hpp"
 
-using unique_SDL_Window = std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)>;
-using unique_SDL_Renderer = std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)>;
-using unique_SDL_Texture = std::unique_ptr<SDL_Texture, decltype(&SDL_DestroyTexture)>;
+#include "smart_SDL.hpp"
 
 class Engine {
 public:
---
+++ src/smart_SDL.hpp
@@ -0,0 +1,11 @@
+#include <SDL3/SDL.h>
+#include <memory>
+
+using unique_SDL_Window = std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)>;
+using unique_SDL_Renderer = std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)>;
+using unique_SDL_Texture = std::unique_ptr<SDL_Texture, decltype(&SDL_DestroyTexture)>;
+using unique_SDL_Surface = std::unique_ptr<SDL_Surface, decltype(&SDL_DestroySurface)>;
+
+using shared_SDL_Texture = std::shared_ptr<SDL_Texture>;
+using shared_TTF_Font = std::shared_ptr<TTF_Font>;
+using shared_Mix_Chunk = std::shared_ptr<Mix_Chunk>;
