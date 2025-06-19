30323306a90864f9328e1101ceced0b6f1d7413f
Programmist <niko4pok@gmail.com>
2025-06-19 16:29:57 +0300

    ifdef optimized

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,17 @@
 ﻿#include "Engine.hpp"
 
-#if defined(_WIN32)
-	#if defined(_DEBUG) || defined(DEBUG) || defined(_CONSOLE)
-		// Debug Windows
-		int main()
-	#else
-		// Release Windows
-		#include <windows.h>
-		int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
-	#endif
-#else
-	// Debug && Release Unix
-	int main()
-#endif
-{
+#if defined(_WIN32) && !(defined(_DEBUG) || defined(DEBUG) || defined(_CONSOLE))
+// Release build on Windows
+#include <windows.h>
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Engine game("Escape of the Fox");
 	game.run();
 	return 0;
 }
+#else
+int main() {
+	Engine game("Escape of the Fox");
+	game.run();
+	return 0;
+}
+#endif
