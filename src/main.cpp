7a9f562ff5471a1b91d63c2e8236adb158df02f9
Programmist <niko4pok@gmail.com>
2025-06-19 16:24:03 +0300

    console now hidden in release version

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 ﻿#include "Engine.hpp"
 
-int main() {
+#if defined(_WIN32)
+	#if defined(_DEBUG) || defined(DEBUG) || defined(_CONSOLE)
+		// Debug Windows
+		int main()
+	#else
+		// Release Windows
+		#include <windows.h>
+		int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+	#endif
+#else
+	// Debug && Release Unix
+	int main()
+#endif
+{
 	Engine game("Escape of the Fox");
 	game.run();
 	return 0;
