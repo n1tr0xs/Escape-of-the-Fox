d6213d948518ac57ce32f4453b0ff1c0f80bebb4
Programmist <niko4pok@gmail.com>
2025-06-23 13:44:35 +0300

    Mushroom::update refactoring

--- src/Mushroom.cpp
+++ src/Mushroom.cpp
@@ -9,17 +9,13 @@ Mushroom::Mushroom(float x, float y, float width, float height, shared_SDL_Textu
 void Mushroom::update(const Uint64 deltaTime) {
 	if (getIsOnGround()) {
 		m_jumpCooldown -= deltaTime;
-	}
-
-	if (m_jumpCooldown <= 0.0f) {
-		if (getIsOnGround()) {
+		if (m_jumpCooldown <= 0.0f) {
 			setVelocityY(-getJumpStrength());
 			setIsOnGround(false);
 			m_jumpCooldown = utils::getRandom(JUMP_COOLDOWN_MIN, JUMP_COOLDOWN_MAX);
 		}
 	}
-
-	 if (!getIsOnGround()) {
+	else {
 		setVelocityY(getVelocityY() + getGravityForce() * deltaTime);
 	}
 }
